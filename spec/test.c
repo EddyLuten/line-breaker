453fb7b06edf2ca98201dd23ee2773093e69a08b
Eddy Luten <eddyluten@gmail.com>
2016-11-14 22:28:34 -0800

    Paragraph support (#4)
    * A work in progress commit for paragraph support.
    
    * Finalized paragraph support

---
+++ spec/test.c
@@ -0,0 +1,8 @@
+/* this is a very long block comment that's will successfully break over multiple lines, but probably not with the fancy syntax that you want. */
+
+            // this is a very long line comment that *is* supported by the line breaker and will successfully break into multiple lines.
+
+/* This is a very long block comment with multiple asterisks that will get completely mangled.
+ * This will get ugly..
+ * Oof.
+ */
